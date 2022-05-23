50bbabdbd2213ea613f6a922061901aad39a5b0d
DongJin Kim <ywpapa@gmail.com>
2022-05-23 10:42:19 +0900

    Initial commit

---
+++ src/main/java/kr/co/bizcore/commapi/CommapiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bizcore.commapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/bizcore/commapi/CommapiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bizcore.commapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
